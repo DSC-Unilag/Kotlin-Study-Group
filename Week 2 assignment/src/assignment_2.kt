722dbea4063c202351d3ffeb03e7dad1f15ab536
DannyRobbs <anerobi86@gmail.com>
2020-10-16 23:02:33 +0100

    re: First Assignment Commit

--- Week 2 assignment/src/assignment_2.kt
+++ Week 2 assignment/src/assignment_2.kt
@@ -8,7 +8,7 @@ fun main(args:Array<String>)
     println("_______________Test1____________________");
     getSquares("2 4 6 8 10 12 14 16 18 20");
     println("\n\n_________________Test2___________________");
-    getSquares("1 2 3 4 5 6 7 8 9 10");
+    getSquares("1 2 3 4 5 6 7 8 9 0");
 }
 
 fun getSquares(word:String)
